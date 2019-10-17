04f11a516f0efc8c7d8cb8f53954fa66f19c8da0
Magnific7 <mjones6944@gmail.com>
2019-10-17 11:56:20 +0200

    image model class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+import datetime as dt
+from tinymce.models import HTMLField
 
 # Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to ='pictures/', )
+    image_name = models.CharField(max_length =50)
+    image_caption = models.TextField()
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default =0)
+    comments = models.TextField(blank=True)
