0bc07a1974166aac4c1f1db974df33ded619228f
Magnific7 <mjones6944@gmail.com>
2019-10-17 12:22:39 +0200

    mprofile model functions

--- insta/models.py
+++ insta/models.py
@@ -4,6 +4,11 @@ import datetime as dt
 from tinymce.models import HTMLField
 
 # Create your models here.
+class Profile(models.Model):
+    bio = models.TextField(max_length=200), null=True, default='bio')
+    profile_photo = models.ImageField(upload_to='pictures/', null=True, blank=True)
+    
+
 
 class Image(models.Model):
     image = models.ImageField(upload_to ='pictures/', )
